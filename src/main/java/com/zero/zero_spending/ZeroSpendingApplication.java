885da174e888ce6b9fe98808c833c6c61901c7a1
boroboro01 <duke7201@naver.com>
2024-10-09 23:04:03 +0900

    init project

---
+++ src/main/java/com/zero/zero_spending/ZeroSpendingApplication.java
@@ -0,0 +1,13 @@
+package com.zero.zero_spending;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeroSpendingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZeroSpendingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zero/zero_spending/ZeroSpendingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zero.zero_spending;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeroSpendingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
