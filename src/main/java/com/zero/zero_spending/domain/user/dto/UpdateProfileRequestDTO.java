27e0a1c310f3b591a77aa07fa9d639a9862b5e64
YeahOut <pt840072@gmail.com>
2024-10-12 14:03:51 +0900

    :sparkles: profile DTO 생성

---
+++ src/main/java/com/zero/zero_spending/domain/user/dto/UpdateProfileRequestDTO.java
@@ -0,0 +1,9 @@
+package com.zero.zero_spending.domain.user.dto;
+
+import lombok.Data;
+
+@Data
+public class UpdateProfileRequestDTO {
+    private String nickname;
+    private String password; 
+}
